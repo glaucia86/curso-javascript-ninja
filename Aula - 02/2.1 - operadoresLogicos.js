db994fbd00fdbda201d37c3bee238881ea41abd7
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-07-24 21:12:53 -0300

    Exercícios e exemplos sobre operadores lógicos.

--- Aula - 02/2.1 - operadoresLogicos.js
+++ Aula - 02/2.1 - operadoresLogicos.js
@@ -8,3 +8,50 @@
  */
 
  
+// 1) Exemplo de Operador Lógico: And &&
+
+> var x = 3;
+undefined
+> var y = 7;
+undefined
+> x === 3;
+true
+> y === 5
+false
+> y === '7'
+false
+> y === 7
+true
+> x === 3 && y ===7
+true
+> x === 3 && y === 4
+false
+
+// 2) Exemplo de Operador Lógico: Or ||
+
+> x
+3
+> y
+7
+> x === 3 || y === 3
+true
+
+
+// 3) Exemplo de Operador Lógico: NOT !
+
+> x === 3
+true
+> ! x === 3
+false
+> x !=== 3
+...
+> x !== 3
+false
+> x != 3
+false
+> x === '3'
+false
+> x != '3'
+false
+> x !== '3'
+true
