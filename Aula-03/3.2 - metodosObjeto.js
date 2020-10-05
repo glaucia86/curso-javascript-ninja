e537953a050c91d5c912957e3c07a7ac323bdfde
glaucia86 <glaucia_lemos86@hotmail.com>
2020-10-05 11:23:52 -0300

    Aula 3.2

---
+++ Aula-03/3.2 - metodosObjeto.js
@@ -0,0 +1,37 @@
+// @ts-nocheck
+/**
+ * Aula: 3.2 - Métodos de Objeto
+ * Author: Glaucia Lemos
+ * Data: 05/10/2020
+ */
+
+const pessoa = {
+    nome: 'Glaucia',
+    idade: 34,
+    peso: 77,
+    altura: 1.56,
+    sexo: 'feminino'
+};
+
+pessoa.cor = 'mulata';
+pessoa.sobrenome = 'Lemos';
+
+// Método:
+pessoa.andar = function() {
+    return 'pessoa andando'
+};
+
+// Método
+pessoa.aniversario = function() {
+    pessoa.idade++;
+};
+
+// Método:
+pessoa.nomeCompleto = function() {
+    return pessoa.nome + ' ' + pessoa.sobrenome;
+};
+
+console.log(pessoa);
+console.log(pessoa.andar());
+console.log(pessoa.nomeCompleto());
+pessoa.aniversario();
