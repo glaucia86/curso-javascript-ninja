e21fa21ef12b1d879551114ca1a1b57cbdde4408
glaucia86 <glaucia_lemos86@hotmail.com>
2020-10-07 12:51:56 -0300

    Exercícios sobre ternario

---
+++ Aula-04/4.2 - ternario.js
@@ -0,0 +1,16 @@
+// @ts-nocheck
+/**
+ * Aula: 4.2 - Condincional Ternário
+ * Author: Glaucia Lemos
+ * Data: 07/10/2020
+ */
+
+// 1 === 2 ? true : false;
+
+const pessoa = {
+    sexo: 'Feminino'
+};
+
+let sexo = pessoa.sexo === 'Feminino' ? 'a' : 'o';
+
+console.log(sexo);
