b011fd2298d04b13ba88e7f11cdcb53e9a59f86c
glaucia86 <glaucia_lemos86@hotmail.com>
2021-01-21 23:10:00 -0300

    Criando os arquivos para o módulo 05

---
+++ Aula-05/5.2 - paramFunctionsArrays.js
@@ -0,0 +1,7 @@
+// @ts-nocheck
+/**
+ * Aula: 5.2 - Parâmetros de Funções com arrays e objetos
+ * Author: Glaucia Lemos
+ * Data: 24/01/2021
+ */
+
---
+++ Aula-05/5.3 - conditionalSwitch.js
@@ -0,0 +1,6 @@
+// @ts-nocheck
+/**
+ * Aula: 5.3 - Operador vírgula e estrutura condicional switch
+ * Author: Glaucia Lemos
+ * Data: 24/01/2021
+ */
