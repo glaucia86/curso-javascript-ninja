6cf459e3e5e105268c86d48a04df3d19689c20d6
glaucia86 <glaucia_lemos86@hotmail.com>
2021-01-24 23:34:57 -0300

    Realização dos exercícios da aula 5.2

--- Aula-05/5.2 - paramFunctionsArrays.js
+++ Aula-05/5.2 - paramFunctionsArrays.js
@@ -5,3 +5,27 @@
  * Data: 24/01/2021
  */
 
+// ==> Exemplo 01: 
+let arrayNumeros = [1,5,6,9];
+
+function myFunction(arg) {
+    return arg;
+};
+
+console.log(myFunction(arrayNumeros)); 
+
+// ==> Exemplo 02: 
+
+let pessoa = {
+    nome: 'Glaucia',
+    curso: 'JavaScript',
+    ninja: true 
+};
+
+console.log(pessoa);
+
+function myFunction2(arg) {
+    return arg;
+};
+
+console.log(myFunction2(pessoa).curso);
